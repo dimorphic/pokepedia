634d7fbdb6ec3bb2209161a4608d2decd7d708b2
Sabin Tudor <sabin@gmx.us>
2016-08-07 15:26:30 +0300

    Create PokemonIcon atom component

---
+++ client/src/components/atoms/PokemonIcon/PokemonIcon.jsx
@@ -0,0 +1,31 @@
+// deps
+import React, { PropTypes } from 'react';
+
+const PokemonIcon = (props) => {
+  const { pokemon, animated, ...otherProps } = props;
+
+  // static PNG
+  const pokemonImage = `http://www.serebii.net/pokemongo/pokemon/${pokemon.pokemonId}.png`;
+
+  // @TODO: add props.animated and switch to GIF ?
+  // const pokemonImage = `http://www.pokestadium.com/sprites/xy/${pokemon.name.toLowerCase()}.gif`;
+  // const pokemonImage = `http://www.pkparaiso.com/imagenes/xy/sprites/animados/${pokemon.name.toLowerCase()}.gif`;
+  // const pokemonImage = `http://pokemon-online.eu/images/pokemon/x-y/animated/${pokemon.pokemonId}.gif`;
+
+  return (
+    <div className="PokemonIcon" {...otherProps}>
+      <img className="img-fluid" src={pokemonImage} alt={pokemon.name} />
+    </div>
+  );
+};
+
+PokemonIcon.propTypes = {
+  pokemon: PropTypes.object,
+  animated: PropTypes.bool
+};
+
+PokemonIcon.defaultProps = {
+  animated: false
+};
+
+export default PokemonIcon;
