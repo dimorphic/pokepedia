64b31389055d2eb9c47d095776866c4da2f688bc
Sabin Tudor <sabin@gmx.us>
2016-08-20 16:02:05 +0300

    Add support to fetch items image to assets downloader

--- server/src/bin/assets-downloader.js
+++ server/src/bin/assets-downloader.js
@@ -1,47 +1,64 @@
 // deps
-// import '../polyfills';
 import { downloadFile, logger, getFileName } from '../utils/helpers';
 import CONFIG from '../config';
 
 // other helpers
 const UTILS = CONFIG.get('utils');
 const PATHS = UTILS.paths;
-const IS_LINK = /^(http|https):\/\//gmi; // HTTP / HTTPS link check
+const IS_LINK = /^(http|https):\/\//; // HTTP / HTTPS link check
 
-// settings
-// const POKEDEX_LINK = `${PATHS.data()}/pokemon/pokedex.en.build.json`;
-const POKEDEX = require(`${PATHS.data()}/pokemon/pokedex`);
-const saveDir = `${PATHS.assets()}/pokemons`;
-
-// @DEBUG
-// http://www.serebii.net/pokemongo/pokemon/<pokemon-id>.png
+// data
+const POKE_MONS = require(`${PATHS.data()}/pokedex`);
+const POKE_ITEMS = require(`${PATHS.data()}/raw/items.json`);
+const saveDir = `${PATHS.assets()}`;
 
 //
 //  Le Downloader
 //
-function batchDownload() {
-  POKEDEX.forEach((pokemon) => {
-    console.log('Pokemon : ', pokemon.name, pokemon.pokemonId);
+function downloadImage(imageURI, savePath) {
+  if (!IS_LINK.test(imageURI)) {
+    console.log('URI is not valid! ', imageURI);
+    return void 0;
+  }
 
-    const imageURI = `http://www.serebii.net/pokemongo/pokemon/${pokemon.pokemonId}.png`;
-    const savePath = `${saveDir}/${pokemon.pokemonId}.png`;
-
-    if (!IS_LINK.test(imageURI)) {
-      console.log('URI is not valid! ', imageURI);
+  // go get'em boy!
+  downloadFile(imageURI, savePath, (err, file) => {
+    if (err) {
+      console.log(err);
       return void 0;
     }
 
-    // go get'em boy!
-    downloadFile(imageURI, savePath, (err, file) => {
-      if (err) {
-        console.log(err);
-        return void 0;
-      }
+    return logger.trace('Done @ ', savePath);
+  });
+}
 
-      return logger.trace('Done @ ', savePath);
-    });
+// @DEBUG
+// http://www.serebii.net/pokemongo/pokemon/<mon-id>.png
+// http://www.serebii.net/pokemongo/items/<item-id>.png
+
+function downloadPokemons() {
+  POKE_MONS.forEach((pokemon) => {
+    console.log('Pokemon : ', pokemon.name, pokemon.pokemonId);
+
+    const imageURI = `http://www.serebii.net/pokemongo/pokemon/${pokemon.pokemonId}.png`;
+    const savePath = `${saveDir}/pokemons/${pokemon.pokemonId}.png`;
+
+    downloadImage(imageURI, savePath);
+  });
+}
+
+function downloadItems() {
+  POKE_ITEMS.forEach((item) => {
+    console.log('Item : ', item);
+
+    const imageURI = item.img;
+    const imageFilename = getFileName(imageURI).toLowerCase();
+    const savePath = `${saveDir}/items/${imageFilename}`;
+
+    downloadImage(imageURI, savePath);
   });
 }
 
 // just do it?
-batchDownload();
+// downloadPokemons();
+downloadItems();
