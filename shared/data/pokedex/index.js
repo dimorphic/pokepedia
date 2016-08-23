2400ca22be4f5bdcf8ca4b491a2dbb176f6e1572
Sabin Tudor <sabin@gmx.us>
2016-08-23 22:21:04 +0300

    Add items and level rewards to Pokedex exporter

--- shared/data/pokedex/index.js
+++ shared/data/pokedex/index.js
@@ -1,1 +1,5 @@
-module.exports = require('./pokedex.en.build.json');
+module.exports = {
+  pokemons: require('./pokemons.en.build.json'),
+  items: require('./items.en.build.json'),
+  levelRewards: require('./level-rewards.en.build.json')
+};
