9e877f98dc9d1d9124030dbc1c7ddffa4f57d4e7
Sabin Tudor <sabin@gmx.us>
2016-09-25 20:56:19 +0300

    Add tapEvent polyfill to server also to get rid of error warning

--- web/src/client/polyfills.js
+++ web/src/client/polyfills.js
@@ -1,8 +1,8 @@
 // deps
 require('es6-promise').polyfill();
 import 'babel-polyfill';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
--- web/src/server/utils/polyfills.js
+++ web/src/server/utils/polyfills.js
@@ -1,3 +1,8 @@
 require('es6-promise').polyfill();
 require('babel-polyfill');
 require('./console');
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
