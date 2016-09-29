057efee530d0a51d5e1580ee1aeda4b40d6bf92a
Sabin Tudor <sabin@gmx.us>
2016-09-29 12:07:38 +0300

    Add meta tags constants

---
+++ web/src/shared/constants/meta.js
@@ -0,0 +1,24 @@
+export default {
+  description: 'Discover Pokemons stats, weaknesses, evolution chances, trainer level ' +
+  'rewards and more. Coming soon: Evolve Calculator, Battle Simulator and live map of ' +
+  'Pokemons locations! Stay tuned!',
+
+  keywords: [
+    'pokepedia',
+    'pokemon',
+    'pokémon',
+    'pokèmon',
+    'pokemon database',
+    'searchable database',
+    'poke find',
+    'pokemon map',
+    'pokemon go',
+    'pokemon go location',
+    'pokemon live map',
+    'pokemon live',
+    'pokemon location',
+    'pokemon stats',
+    'pokemon weaknesses',
+    'pokemon evolution'
+  ]
+};
